e8ba66c7ad15f9765b0ecbcc306323d806df5a8e
Magdiel Campelo Alves de Sousa <magdiel.camp@gmail.com>
2018-01-13 13:44:29 -0300

    tela de detalhe

---
+++ app/src/main/java/ninja/rasga/mambeerasganinja/DetalheActivity.java
@@ -0,0 +1,17 @@
+package ninja.rasga.mambeerasganinja;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
