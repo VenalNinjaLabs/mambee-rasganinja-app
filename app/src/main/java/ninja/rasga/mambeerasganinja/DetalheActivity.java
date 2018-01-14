462a6eac94a0500e692bc74ff8da43d28117a092
Magdiel Campelo Alves de Sousa <magdiel.camp@gmail.com>
2018-01-14 10:42:45 -0300

    detalhes mockup

--- app/src/main/java/ninja/rasga/mambeerasganinja/DetalheActivity.java
+++ app/src/main/java/ninja/rasga/mambeerasganinja/DetalheActivity.java
@@ -3,8 +3,14 @@ package ninja.rasga.mambeerasganinja;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DetalheActivity extends AppCompatActivity {
+
+    private ModeloRasgada rasgadaDetalhada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,25 @@ public class DetalheActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        rasgadaDetalhada = new ModeloRasgada("Fulano de tal",
+                "Picos",
+                "IFPI",
+                "Fulano de tal tava andando de bicicleta e levou um tombo",
+                10,
+                5,
+                Calendar.getInstance().getTime());
+
+        TextView nome = findViewById(R.id.nome);
+        TextView cidade = findViewById(R.id.cidade);
+        TextView referencia = findViewById(R.id.referencia);
+        TextView comentario = findViewById(R.id.comentario);
+        TextView avatar = findViewById(R.id.avatar);
+
+        nome.setText(rasgadaDetalhada.getNome());
+        cidade.setText(rasgadaDetalhada.getCidade());
+        referencia.setText(rasgadaDetalhada.getReferencia());
+        comentario.setText(rasgadaDetalhada.getComentario());
+        avatar.setText(rasgadaDetalhada.getNome().toUpperCase().charAt(0));
     }
 }
